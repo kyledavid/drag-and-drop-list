002a2cbc4d8e08398a535e2b9b46dcb508e89ad6
Kyle Steinle <kyledavid022@gmail.com>
2017-06-02 09:28:54 -0700

    list spots added. ability to add to specific slots added

--- js/drag.js
+++ js/drag.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	var yCord = 0; // current cursor y position
 	var moving = false; // enables/disables box movement
 	var draggables = document.querySelectorAll('.draggable'); // get all our draggable items
+	var listSlots = [[1],[2],[3],[4]];
+	var spotsToSnap = document.querySelectorAll('.snap-to-me');
 
 	draggables.forEach(function(box) {	
 		// initiate drag function when draggable item is clicked
@@ -22,28 +24,15 @@ document.addEventListener('DOMContentLoaded', function() {
 		moving = true;
 		// give dragged item higest z index so others aren't accidentally selected
 		block.classList.add('dragged');
+		// initial positioning of dragged item
+		positionItemOnCursor(event, block);
+		// remove in-list class
+		block.classList.remove('in-list');
 		// update the position of the block when client mouse moves
 		block.addEventListener('mousemove', function(event) {
 			// run if movement is allowed
 			if (moving) {
-				// account for the dimensions of our box, divied them by 2 so box will be centered on cursor
-				var xOffset = block.offsetWidth / 2;
-				var yOffset = block.offsetHeight / 2;
-				// get the position of the cursor relative to the viewport
-				xCord = event.clientX;
-				yCord = event.clientY;
-				// if scrolled, add the vertical scroll to the y coordinate to allow absolute positioning to work correctly
-				if (window.scrollY) {yCord += window.scrollY;}
-				// display our coordinates for testing
-				document.getElementById('x-coord').textContent = xCord + 'px';
-				document.getElementById('y-coord').textContent = yCord + 'px';
-				// subtract offsets from cursor to center box over cursor
-				yCord = yCord - yOffset;
-				xCord = xCord - xOffset;
-				// create inline styles for positioning box
-				var pos = 'top: ' + yCord + "px; left: " + xCord + "px;";
-				// set inline styles for box
-				block.setAttribute('style', pos);
+				positionItemOnCursor(event, block);
 			}
 		});
 	}
@@ -55,19 +44,64 @@ document.addEventListener('DOMContentLoaded', function() {
 		selection.classList.remove('dragged');
 		// mouse is up so we should no longer allow movement
 		moving = false;
+		xCord = event.clientX;
 		// check if coordinates are within range 
-		if (xCord > 482 && xCord < 1036 && yCord <= 194 && yCord >= 62) {
-			// get position of snap to me element
-			var snapTo = document.getElementById('snap-to-me');
-			var snapBox = snapTo.getBoundingClientRect();
-			// selection is within coordinate thresholds, move it into the center of snap-to box
-			var selectionX = (snapBox.left + snapBox.right) / 2 - 126;
-			var selectionY = (snapBox.top + snapBox.bottom) / 2 - 17;
-			// create our styles to be added to selection
-			var pos = 'top: ' + selectionY + 'px; left: ' + selectionX + 'px;';
-			// add styles to selection
-			selection.setAttribute('style', pos);
+		if (xCord >= 482 && xCord <= 1036 && yCord >= 62 && yCord <= 181) {
+			// unset positioning
+			selection.setAttribute('style', 'top: unset; left: unset;');
+			// stylu it for sitting in list
+			selection.classList.add('in-list');
+			// append it to the appropriate spot
+			spotsToSnap[0].appendChild(selection);
+		} else if(xCord > 482 && xCord < 1036 && yCord >= 182 && yCord <= 282){
+			// unset positioning
+			selection.setAttribute('style', 'top: unset; left: unset;');
+			// stylu it for sitting in list
+			selection.classList.add('in-list');
+			// append it to the appropriate spot
+			spotsToSnap[1].appendChild(selection);
 		}
+		else if(xCord >= 482 && xCord <= 1036 && yCord >= 283 && yCord <= 383){
+			// unset positioning
+			selection.setAttribute('style', 'top: unset; left: unset;');
+			// stylu it for sitting in list
+			selection.classList.add('in-list');
+			// append it to the appropriate spot
+			spotsToSnap[2].appendChild(selection);
+		}
+		else if(xCord >= 482 && xCord <= 1036 && yCord >= 384 && yCord <= 505){
+			// unset positioning
+			selection.setAttribute('style', 'top: unset; left: unset;');
+			// stylu it for sitting in list
+			selection.classList.add('in-list');
+			// append it to the appropriate spot
+			spotsToSnap[3].appendChild(selection);
+		}
+		else {
+			// unset positioning
+			selection.setAttribute('style', 'top: unset; left: unset;');	
+		}
+	}
+
+	function positionItemOnCursor(event, block) {
+		// account for the dimensions of our box, divied them by 2 so box will be centered on cursor
+		var xOffset = block.offsetWidth / 2;
+		var yOffset = block.offsetHeight / 2;
+		// get the position of the cursor relative to the viewport
+		xCord = event.clientX;
+		yCord = event.clientY;
+		// if scrolled, add the vertical scroll to the y coordinate to allow absolute positioning to work correctly
+		if (window.scrollY) {yCord += window.scrollY;}
+		// display our coordinates for testing
+		document.getElementById('x-coord').textContent = xCord + 'px';
+		document.getElementById('y-coord').textContent = yCord + 'px';
+		// subtract offsets from cursor to center box over cursor
+		yCord = yCord - yOffset;
+		xCord = xCord - xOffset;
+		// create inline styles for positioning box
+		var pos = 'top: ' + yCord + "px; left: " + xCord + "px;";
+		// set inline styles for box
+		block.setAttribute('style', pos);
 	}
 
 
