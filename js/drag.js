4f217047df16d0ca02a957217bfc9bbbdb774be2
Kyle Steinle <kyledavid022@gmail.com>
2017-06-03 09:25:17 -0700

    some comment refactoring complete. App works

--- js/drag.js
+++ js/drag.js
@@ -2,36 +2,36 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-	var xCord = 0; // current cursor x position
-	var yCord = 0; // current cursor y position
-	var moving = false; // enables/disables box movement
-	var draggables = document.querySelectorAll('.draggable'); // get all our draggable items
-	var spotsToSnap = document.querySelectorAll('.snap-to-me'); // get all the list spots
+	var userX = 0; 
+	var userY = 0; 
+	var permitMovement = false; 
+	var listItems = document.querySelectorAll('.list-item'); 
+	var listSpots = document.querySelectorAll('.snap-to-me'); 
 
-	draggables.forEach(function(box) {	
-		// initiate drag function when draggable item is clicked
-		box.addEventListener('mousedown', initDrag); 
-		// stop drag of item on mouseup
-		box.addEventListener('mouseup', endDrag); 
+	listItems.forEach(function(listItem) {	
+
+		listItem.addEventListener('mousedown', initDrag); 
+		listItem.addEventListener('mouseup', endDrag); 
+
 	});
 
-	// begin dragging when item is clicked
-	function initDrag(event) {
-		// select target to be dragged
-		var block = event.target; 
-		// because the mouse was clicked, allow movement
-		moving = true;
-		// give dragged item higest z index so others aren't accidentally selected
-		block.classList.add('dragged');
-		// initial positioning of dragged item
-		positionItemOnCursor(event, block);
-		// remove in-list class
-		block.classList.remove('in-list');
-		// update the position of the block when client mouse moves
-		block.addEventListener('mousemove', function(event) {
-			// run if movement is allowed
-			if (moving) {
-				positionItemOnCursor(event, block);
+
+	function initDrag(mouseEvent) {
+
+		var listItem = mouseEvent.target; 
+
+		permitMovement = true;
+
+		listItem.classList.add('dragged');
+
+		positionItemOnCursor(mouseEvent, listItem);
+
+		listItem.classList.remove('in-list');
+
+		listItem.addEventListener('mousemove', function(moveEvent) {
+
+			if (permitMovement) {
+				positionItemOnCursor(moveEvent, listItem);
 			}
 		});
 	}
@@ -42,40 +42,40 @@ document.addEventListener('DOMContentLoaded', function() {
 		var selection = event.target;
 		selection.classList.remove('dragged');
 		// mouse is up so we should no longer allow movement
-		moving = false;
-		xCord = event.clientX;
-		yCord = event.clientY;
+		permitMovement = false;
+		userX = event.clientX;
+		userY = event.clientY;
 		// check if coordinates are within range 
-		if (xCord >= 482 && xCord <= 1036 && yCord >= 62 && yCord <= 181) {
+		if (userX >= 482 && userX <= 1036 && userY >= 62 && userY <= 181) {
 			// unset positioning
 			selection.setAttribute('style', 'top: unset; left: unset;');
 			// stylu it for sitting in list
 			selection.classList.add('in-list');
 			// append it to the appropriate spot
-			spotsToSnap[0].appendChild(selection);
-		} else if(xCord > 482 && xCord < 1036 && yCord >= 182 && yCord <= 282){
+			listSpots[0].appendChild(selection);
+		} else if(userX > 482 && userX < 1036 && userY >= 182 && userY <= 282){
 			// unset positioning
 			selection.setAttribute('style', 'top: unset; left: unset;');
 			// stylu it for sitting in list
 			selection.classList.add('in-list');
 			// append it to the appropriate spot
-			spotsToSnap[1].appendChild(selection);
+			listSpots[1].appendChild(selection);
 		}
-		else if(xCord >= 482 && xCord <= 1036 && yCord >= 283 && yCord <= 383){
+		else if(userX >= 482 && userX <= 1036 && userY >= 283 && userY <= 383){
 			// unset positioning
 			selection.setAttribute('style', 'top: unset; left: unset;');
 			// stylu it for sitting in list
 			selection.classList.add('in-list');
 			// append it to the appropriate spot
-			spotsToSnap[2].appendChild(selection);
+			listSpots[2].appendChild(selection);
 		}
-		else if(xCord >= 482 && xCord <= 1036 && yCord >= 384 && yCord <= 505){
+		else if(userX >= 482 && userX <= 1036 && userY >= 384 && userY <= 505){
 			// unset positioning
 			selection.setAttribute('style', 'top: unset; left: unset;');
 			// stylu it for sitting in list
 			selection.classList.add('in-list');
 			// append it to the appropriate spot
-			spotsToSnap[3].appendChild(selection);
+			listSpots[3].appendChild(selection);
 		}
 		else {
 			// unset positioning
@@ -90,18 +90,18 @@ document.addEventListener('DOMContentLoaded', function() {
 		var xOffset = block.offsetWidth / 2;
 		var yOffset = block.offsetHeight / 2;
 		// get the position of the cursor relative to the viewport
-		xCord = event.clientX;
-		yCord = event.clientY;
+		userX = event.clientX;
+		userY = event.clientY;
 		// if scrolled, add the vertical scroll to the y coordinate to allow absolute positioning to work correctly
-		if (window.scrollY) {yCord += window.scrollY;}
+		if (window.scrollY) {userY += window.scrollY;}
 		// display our coordinates for testing
-		document.getElementById('x-coord').textContent = xCord + 'px';
-		document.getElementById('y-coord').textContent = yCord + 'px';
+		document.getElementById('x-coord').textContent = userX + 'px';
+		document.getElementById('y-coord').textContent = userY + 'px';
 		// subtract offsets from cursor to center box over cursor
-		yCord = yCord - yOffset;
-		xCord = xCord - xOffset;
+		userY = userY - yOffset;
+		userX = userX - xOffset;
 		// create inline styles for positioning box
-		var pos = 'top: ' + yCord + "px; left: " + xCord + "px;";
+		var pos = 'top: ' + userY + "px; left: " + userX + "px;";
 		// set inline styles for box
 		block.setAttribute('style', pos);
 	}
